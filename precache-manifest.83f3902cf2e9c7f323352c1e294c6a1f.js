ff8af4fe97a98bcf37b788385e8d028a7a8d136c
Louis Hsiao <louis.hsiao@d8ai.com>
2020-02-21 19:23:43 +0800

    Updates

---
+++ precache-manifest.83f3902cf2e9c7f323352c1e294c6a1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad294394d0edf1504cc31a1169cec42",
+    "url": "/shoppingcart-demo/index.html"
+  },
+  {
+    "revision": "98dfe95c11f87ecc299e",
+    "url": "/shoppingcart-demo/static/js/2.9e0e1f98.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/shoppingcart-demo/static/js/2.9e0e1f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b6e5d84bc813aa5890",
+    "url": "/shoppingcart-demo/static/js/main.64fd6fec.chunk.js"
+  },
+  {
+    "revision": "75853c7c446c6745f205",
+    "url": "/shoppingcart-demo/static/js/runtime-main.d878d1d0.js"
+  }
+]);
