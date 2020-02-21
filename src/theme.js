67079e0818d2c7b1cb4eddae21274d86233608ce
Louis Hsiao <louis.hsiao@d8ai.com>
2020-02-21 19:16:31 +0800

    feat: custom theme

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,12 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export default createMuiTheme({});
+export default createMuiTheme({
+  palette: {
+    secondary: {
+      main: '#fff',
+    },
+    background: {
+      cardBg: 'rgb(59, 60, 67)',
+    }
+  }
+});
